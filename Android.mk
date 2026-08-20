09c67978f5beb05efff6ab7c226c5a8b6c9ae635
Wiktor Rudzki <zlewchan@outlook.com>
2026-08-20 22:44:20 +0200

    nx659j: Import firmware

--- Android.mk
+++ Android.mk
@@ -3,3 +3,25 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),nx659j)
+
+$(call add-radio-file-sha1-checked,radio/BTFM.bin,8d409e013d37397f86abf75f3d08dd63482df6fe)
+$(call add-radio-file-sha1-checked,radio/NON-HLOS.bin,ffbb9a873a9d3386c360972f5f8343f8720cd933)
+$(call add-radio-file-sha1-checked,radio/aop.mbn,9851efb2d7c91133555b1a3bf6fbc2f4508adace)
+$(call add-radio-file-sha1-checked,radio/cmnlib.mbn,b2f69a501837262008d043a76be07484b6eb5ea0)
+$(call add-radio-file-sha1-checked,radio/cmnlib64.mbn,f839a1af3f21667732fbd2b7c6263d8ed265281d)
+$(call add-radio-file-sha1-checked,radio/devcfg.mbn,5a83db23f160a32529c05c10c404e2c4669ba686)
+$(call add-radio-file-sha1-checked,radio/dspso.bin,aed28fb777b2ad90c95e0d040eb83f057f837ba0)
+$(call add-radio-file-sha1-checked,radio/hyp.mbn,e6caecbcb4b752f895957ea99ffd3bcb88cc849e)
+$(call add-radio-file-sha1-checked,radio/imagefv.elf,0d9e8ba2bbc5cc9621d8e8908c4ebe7114c8380c)
+$(call add-radio-file-sha1-checked,radio/multi_image.mbn,be57aa98598fbf6fbd068370d98dc109ba7d0827)
+$(call add-radio-file-sha1-checked,radio/parameter.img,c66326912a9bb12523c228ca9feb097dc39de5b1)
+$(call add-radio-file-sha1-checked,radio/qupv3fw.elf,0af57b91e5fb76769ec3ba38687c3807eb23c4e4)
+$(call add-radio-file-sha1-checked,radio/splash.img,925f68ff47c7e588c50539f49061b73a8fd88cb2)
+$(call add-radio-file-sha1-checked,radio/tz.mbn,81a9f161189d80cd51e8c5bc293117c49b84e8e8)
+$(call add-radio-file-sha1-checked,radio/uefi_sec.mbn,9b9eb9d32c9b8d07bcd41bf353ac5939bc660188)
+$(call add-radio-file-sha1-checked,radio/xbl.elf,2b32ff598ec2443eabcea1777ca4852b68712f05)
+$(call add-radio-file-sha1-checked,radio/xbl_config.elf,2e44846989e850ef1b1fe54f22f340305d9d972b)
+
+endif
